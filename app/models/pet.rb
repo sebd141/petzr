57967e6c1201f572cbdae54e152e2eaa17e2f2d5
sebastien dellis <sebastien.dellis@gmail.com>
2022-06-01 15:40:34 +0200

    navbar fixed

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,3 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  validates :breed
 end
--- db/migrate/20220601124207_change_type_of_service_to_array.rb
+++ db/migrate/20220601124207_change_type_of_service_to_array.rb
@@ -1,6 +1,6 @@
 class ChangeTypeOfServiceToArray < ActiveRecord::Migration[6.1]
   def change
     remove_column :users, :type_of_service
-    add_column :users, :type_of_service, :string, array: true, default: []
+    add_column :users, :type_of_service , :string, array: true, default: []
   end
 end
