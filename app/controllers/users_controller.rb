e80009bb45f916180a4d5d961edef1bdca30befe
Val Smets <valsmets@hotmail.com>
2022-06-02 15:24:47 +0200

    user petsitter form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def show
@@ -13,15 +16,21 @@ class UsersController < ApplicationController
   def become_pet_sitter
     @user = current_user
     @user.update(pet_sitters_status: true)
-    @user.save
-    redirect_to new_pet_sitter_form_path
+     if @user.save
+      redirect_to new_pet_sitter_form_path
+     else
+      render :edit
+     end
   end
 
 
   def add_pet_sitter_details
     @user = current_user
-    @user.update(pet_sitters_status: true)
-    @user.save
-    redirect_to new_pet_sitter_form_path
+
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:description, :short_desc, :location, { type_of_pet: [] }, { type_of_service: [] }, :price)
   end
 end
