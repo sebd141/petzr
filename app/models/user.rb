141834f18a3e5b14c83113475443d6ebb06319eb
Archibald Saen <archibaldsaen98@gmail.com>
2022-06-02 17:15:54 +0200

    moved the show to dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :reviews_recieved, class_name: 'Review', foreign_key: :reciever_id, primary_key: :id
   has_many :reviews_writer, class_name: 'Review', foreign_key: :writer_id, primary_key: :id
+
   has_many :contracts_pet_sitter, class_name: 'Contract', foreign_key: :id_pet_sitter, primary_key: :id
   has_many :contracts_user, class_name: 'Contract', foreign_key: :user_id, primary_key: :id
   has_many :posts
