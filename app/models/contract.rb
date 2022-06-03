1efecc671cb18454f02379b0b0642f14418a7a57
Archibald Saen <archibaldsaen98@gmail.com>
2022-06-03 12:52:57 +0200

    added the basic chatroom without javascript

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,5 +2,5 @@ class Contract < ApplicationRecord
   # validates :id_pet_sitter, allow_nil: true
   belongs_to :pet_sitter, class_name: 'User', foreign_key: 'id_pet_sitter', primary_key: :id, optional: true
   belongs_to :user, class_name: 'User', foreign_key: 'user_id', primary_key: :id, optional: false
-  has_many :posts
+  has_many :posts, dependent: :destroy
 end
