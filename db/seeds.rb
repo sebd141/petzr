dba366d063321474b889fcaea5e9bafa5d73992f
sebastien dellis <sebastien.dellis@gmail.com>
2022-06-07 10:41:35 +0200

    added new db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ puts "Cleaning database..."
 
 puts "Creating User for testing"
 1.times do (
-@bob = User.new({ email: "a@a.com", password: "lewagon", first_name: "Archi", last_name: "Saen", pet_sitters_status: false})
+@bob = User.new({ email: "a@a.com", password: "lewagon", first_name: "Archi", last_name: "Saen", location: "Rue du try bara 2, 1380 Lasne", pet_sitters_status: false})
 file_bob = URI.open('https://res.cloudinary.com/sebd141/image/upload/v1653047425/kknxc2lusyegbjjjoqia.jpg')
 @bob.photo.attach(io: file_bob, filename: 'profile.jpeg', content_type: 'image/jpeg')
 @bob.save
@@ -52,12 +52,12 @@ puts "Creating Pet sitters"
     pet_sitters_status: true,
     first_name: ['Seb', 'Val', 'Archi', 'Antoine', 'Nikos', 'Jack', 'Valentin', 'Alexandre'].sample,
     last_name: ['RobinHood', 'Pocaonthas', 'Simba', 'Snow', 'Targaryan', 'Conor', 'Smith', 'Dupont'].sample,
-    location: ['Be Central, Brussels', 'Rue Neuve, 1000 Brussels', 'rue du progrès, 100, Brussels', 'Bozar, 1000 Brussels', 'Avenue louise 86, Brussels', 'Atomium, Brussels', 'Avenue Louise 203, 1050 Ixells'].sample,
+    location: ['Be Central, Brussels', 'Rue Neuve, 1000 Brussels', 'rue du progrès, 100, Brussels', 'Bozar, 1000 Brussels', 'Avenue louise 86, Brussels', 'Atomium, Brussels', 'Avenue Louise 203, 1050 Brussels', 'Café bota, brussels'].sample,
     price: [10.5, 5.5, 6.5, 7.5].sample,
     type_of_pet: ["cat", "dog", "bird", "fish", "snake"].sample(rand(2..4)),
     type_of_service: ["walk", "feed", "medication"].sample(rand(2..3)),
     description: Faker::Lorem.paragraph,
-    short_desc: "Hello I'm User, I will take care of your pet!",
+    short_desc: ["Hello! I will take care of your pet! I love pet since 1O years", "Hello, I'm 25 years old. I love pet & I want to earn pocket money by taking care of your pet"].sample
   )
   file = URI.open(['https://res.cloudinary.com/sebd141/image/upload/v1654172150/rabit_kid1_wmlcdo.jpg',
                     'https://res.cloudinary.com/sebd141/image/upload/v1654172150/human_rabbit2_xvpcee.jpg',
